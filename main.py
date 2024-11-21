a754f3a46b4d2d245adc591a93261b4e697a9d5e
k-malek <krzysztof.malek.90@gmail.com>
2024-11-21 11:51:49 +0100

    adding popup handler

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
+from time import sleep
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from popup_utils import PopupHandler
 
 #Keep browser open
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option('detach',True)
 
 driver = webdriver.Chrome(options=chrome_options)
-driver.get('https://www.youtube.com/')
+
+driver.set_window_position(-1000, 0)
+driver.maximize_window()
+
+driver.get('https://orteil.dashnet.org/cookieclicker/')
+
+popup_handler=PopupHandler(driver)
+
+popup_handler.close_popups()
+sleep(1)
+popup_handler.close_notes()
+
+GAME=driver.find_element(By.ID,'game')
+
+#driver.close()
---
+++ popup_utils.py
@@ -0,0 +1,26 @@
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import StaleElementReferenceException
+
+class PopupHandler:
+    
+    def __init__(self,driver:webdriver) -> None:
+        self.DRIVER = driver
+
+    def close_popups(self):
+        self.DRIVER.find_element(By.CSS_SELECTOR,'[aria-label="Manage options"]').click()
+        self.DRIVER.find_element(By.CSS_SELECTOR,'[aria-label="Confirm choices"]').click()
+        sleep(0.4)
+        self.DRIVER.find_element(By.CLASS_NAME,'cc_btn_accept_all').click()
+        self.DRIVER.find_element(By.ID,'langSelect-EN').click()
+        sleep(0.6)
+        self.DRIVER.find_element(By.XPATH,'/html/body/div[2]').shadow_root.find_element(By.CLASS_NAME,'ft-reg-bubble-close').click()
+
+    def close_notes(self):
+        for note in self.DRIVER.find_element(By.ID,'notes').find_elements(By.CSS_SELECTOR,'div[id*=note]'):
+            try:
+                note.find_element(By.CSS_SELECTOR,'div.close').click()
+            except StaleElementReferenceException:
+                print(f'Element {note} has been hidden')
+                continue
